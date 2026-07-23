c43a63d48d56e2416baaf8569c7f8e82bd4843d7
cyan1dealex <cyan1de.wot@mail.ru>
2026-07-23 16:46:20 +0300

    feat(js-ts-task-4): implement core crud operations

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,29 @@
 import { Todo } from './types';
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+  return [...state, todo]
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+  if (!state.some(item => item.id === id)) throw new Error ("Todo not found")
+
+  return state.map(todo => {
+    if (todo.id === id) {
+      return {
+        ...todo,
+        ...update
+      }
+    }
+    return todo
+  })
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  if (!state.some(item => item.id === id)) throw new Error ("Todo not found")
+
+  return state.filter(todo => todo.id !== id)
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  return state.find(todo => todo.id === id)
 }
