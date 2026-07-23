196f7fdda98bc393fc6019de8c66b45c992dbeca
cyan1dealex <cyan1de.wot@mail.ru>
2026-07-23 15:58:05 +0300

    feat(js-ts-task-2): create helpers to work with arrays.

--- JS-TS/solutions/array-helpers.ts
+++ JS-TS/solutions/array-helpers.ts
@@ -2,22 +2,75 @@
 // Task 02: Mini functional–utility library
 // All helpers are declared but not implemented.
 
+function validateSource<T>(source: readonly T[] | null | undefined): asserts source is readonly T[] {
+  if (source === null || source === undefined) {
+    throw new TypeError("source array can't be null or undefined!")
+  }
+}
+
 export function mapArray<T, R>(source: readonly T[], mapper: (item: T, index: number) => R): R[] {
-  throw new Error('mapArray: not implemented');
+  validateSource(source)
+
+  const result: R[] = [];
+  for (let i = 0; i < source.length; i++) {
+    result.push(mapper(source[i], i))
+  }
+  return result
 }
 
 export function filterArray<T>(source: readonly T[], predicate: (item: T, index: number) => boolean): T[] {
-  throw new Error('filterArray: not implemented');
+  validateSource(source)
+
+  const result: T[] = [];
+  for (let i = 0; i < source.length; i++) {
+    if (predicate(source[i], i)) {
+      result.push(source[i])
+    }
+  }
+  return result;
 }
 
 export function reduceArray<T, R>(source: readonly T[], reducer: (acc: R, item: T, index: number) => R, initial: R): R {
-  throw new Error('reduceArray: not implemented');
+  validateSource(source)
+
+  let acc: R = initial
+  for (let i = 0; i < source.length; i++) {
+    acc = reducer(acc, source[i], i)
+  }
+  return acc
 }
 
 export function partition<T>(source: readonly T[], predicate: (item: T) => boolean): [T[], T[]] {
-  throw new Error('partition: not implemented');
+  validateSource(source)
+
+  const passed: T[] = []
+  const failed: T[] = []
+
+  for(let i = 0; i < source.length; i++) {
+    if (predicate(source[i])) {
+      passed.push(source[i])
+    } else {
+      failed.push(source[i])
+    }
+  }
+  return [passed, failed]
 }
 
 export function groupBy<T, K extends PropertyKey>(source: readonly T[], keySelector: (item: T) => K): Record<K, T[]> {
-  throw new Error('groupBy: not implemented');
+  validateSource(source)
+
+  const result = {} as Record<K, T[]>;
+
+  for (let i = 0; i < source.length; i++) {
+    const item = source[i];
+    const key = keySelector(item);
+
+    if (!result[key]) {
+      result[key] = [];
+    }
+
+    result[key].push(item);
+  }
+
+  return result;
 }
