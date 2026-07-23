3832ab37715cd7ae01b1ff6d9d9e336121cb62ed
cyan1dealex <cyan1de.wot@mail.ru>
2026-07-23 16:23:50 +0300

    feat(js-ts-task-3): implement createTodo factory function

--- JS-TS/solutions/todo-factory.ts
+++ JS-TS/solutions/todo-factory.ts
@@ -3,5 +3,12 @@ import { Todo, NewTodo, TodoStatus } from './types';
 let nextId = 1;
 
 export function createTodo(input: NewTodo): Todo {
-  throw new Error('createTodo: not implemented');
+  const todoCard: Todo = {
+    id: nextId++,
+    title: input.title,
+    status: TodoStatus.PENDING,
+    description: input.description,
+    createdAt: new Date()
+  }
+  return todoCard
 }
