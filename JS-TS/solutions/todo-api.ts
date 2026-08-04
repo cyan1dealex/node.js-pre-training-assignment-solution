cd29541a8bf6eef9e39a11e58ddd6fbd88bebc7d
cyan1dealex <cyan1de.wot@mail.ru>
2026-08-04 21:54:08 +0300

    feat(js-ts-task-6): simulate remote API

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,22 +1,57 @@
 import { InMemoryRepository } from './repository';
-import { Todo, NewTodo } from './types';
+import { Todo, NewTodo, TodoStatus } from './types';
+
+export class TodoNotFoundError extends Error {
+  constructor(id: number) {
+    super(`Task with id ${id} not found.`)
+    this.name = 'TodoNotFoundError'
+  }
+}
 
 export class TodoApi {
   private repo = new InMemoryRepository<Todo>();
 
+  private nextId = 1
+
+  private delay(): Promise<void> {
+    const ms = 450
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
   async getAll(): Promise<Todo[]> {
-    throw new Error('getAll: not implemented');
+    await this.delay()
+
+    return this.repo.findAll()
   }
 
   async add(newTodo: NewTodo): Promise<Todo> {
-    throw new Error('add: not implemented');
+    await this.delay();
+
+    const todo: Todo = {
+      ...newTodo,
+      id: this.nextId++,
+      status: newTodo.status ?? TodoStatus.PENDING,
+      createdAt: new Date(),
+    };
+
+    return this.repo.add(todo);
   }
 
   async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
-    throw new Error('update: not implemented');
+    await this.delay();
+
+    const isExist = this.repo.findById(id);
+    if (!isExist) throw new TodoNotFoundError(id)
+
+    return this.repo.update(id, update)
   }
 
   async remove(id: number): Promise<void> {
-    throw new Error('remove: not implemented');
+    await this.delay();
+
+    const isExist = this.repo.findById(id);
+    if (!isExist) throw new TodoNotFoundError(id)
+
+    return this.repo.remove(id)
   }
 }
