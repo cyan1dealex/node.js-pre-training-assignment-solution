d540a8d46e9c32857fd635ec8a56ac78dd508cd0
cyan1dealex <cyan1de.wot@mail.ru>
2026-08-12 21:36:54 +0300

    feat(js-ts-task-8): compose services with dependency injection

--- JS-TS/solutions/todo-service.ts
+++ JS-TS/solutions/todo-service.ts
@@ -1,18 +1,47 @@
 import { TodoApi } from './todo-api';
-import { Todo } from './types';
+import { Todo, TodoStatus } from './types';
 
 export class TodoService {
   constructor(private readonly api: TodoApi) { }
 
   async create(title: string, description = ''): Promise<Todo> {
-    throw new Error('create: not implemented');
+    if (!title || title.trim() === ''){
+      throw new Error('Title cannot be empty')
+    }
+
+    return this.api.add({
+      title: title.trim(),
+      description: description.trim()
+    })
   }
 
   async toggleStatus(id: number): Promise<Todo> {
-    throw new Error('toggleStatus: not implemented');
+    const allTodos = await this.api.getAll()
+    const neededTodo = allTodos.find(element => element.id === id)
+
+    if (!neededTodo) {
+      throw new Error(`Todo with id ${id} is not found`)
+    }
+
+    return this.api.update(id, {
+      status: neededTodo.status === TodoStatus.PENDING ? TodoStatus.COMPLETED : TodoStatus.PENDING
+    })
   }
 
   async search(keyword: string): Promise<Todo[]> {
-    throw new Error('search: not implemented');
+    const allTodos = await this.api.getAll()
+    
+    if (!keyword || keyword.trim() === '') {
+      return allTodos
+    }
+
+    const keywordLowercase = keyword.toLowerCase().trim()
+
+    return allTodos.filter(element => {
+      const matchTitle = element.title.toLowerCase().includes(keywordLowercase)
+      const matchDescription = element.description?.toLowerCase().includes(keywordLowercase)
+
+      return matchTitle || matchDescription
+    })
   }
 }
